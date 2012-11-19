5467b1589fcc4cc5623844b7c3edc34d41d42e3e
mccoy sammy-boy <mckizboi@gmail.com>
2012-11-19 16:01:26 +0000

    Done with the ex app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExApp::Application.config.secret_token = '0f2aee73bbe6328dc0ace42423479604ad9b2f02fd398718031f6508566494bfb047d59a8378d5790735155063be036b159403f40368329f17a0903ccb2031ca'
